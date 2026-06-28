fea3baf678313aac7c54208fc14e8e82b75d3610
ojgabrielleal <joaogabrieel79@gmail.com>
2026-06-27 22:02:46 -0300

    Initial Spring Boot project

---
+++ src/main/java/com/sonari/SonariApplication.java
@@ -0,0 +1,13 @@
+package com.sonari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonari/SonariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
